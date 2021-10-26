cc724ee047c0a3647386ec6a428a6f408984a485
박상민 <baksangmin95@gmail.com>
2021-10-26 19:46:38 -0400

    Add project files.

---
+++ Quadratic Formula Solver.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+
+    int aTrm{ 0 }; //The a term
+    int bTrm{ 0 }; //The b term
+    int cTrm{ 0 }; //The c term
+    int negB{ 0 }; //The -b +/- sqrt() thing
+
+    std::cout << "Please imput the [a] term\n";
+        std::cin >> aTrm;
+    std::cout << "Please imput the [b] term\n";
+        std::cin >> bTrm;
+    std::cout << "Please imput the [c] term\n";
+        std::cin >> cTrm;
+
+    negB = aTrm * -1
+
+}
